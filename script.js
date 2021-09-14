70fc431386fd50c7298ecad5ef286d069dec0562
Dharma <dharmaguadeloupe@gmail.com>
2021-09-14 14:40:36 +0100

    Updated header to display as fixed

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const allEpisodes = getAllEpisodes();
-
+ 
 //You can edit ALL of the code here
 function setup() {
   makePageForEpisodes(allEpisodes);
@@ -18,9 +18,10 @@ function makePageForEpisodes(episodeList) {
   /*Episode Title*/
   const episodeTitle = episodesCard.appendChild(document.createElement("h3"));
   episodeTitle.setAttribute("class", "episodeTitle");
+  const episodeName = episode.name;
   const season = episode.season.toString().padStart(2, "0");
   const episodeNumber = episode.number.toString().padStart(2, "0");
-  episodeTitle.textContent = `${episode.name} - S${season}E${episodeNumber}`; 
+  episodeTitle.textContent = `${episodeName} - S${season}E${episodeNumber}`; 
 
   /*Episode Image*/
   const image = episodesCard.appendChild(document.createElement("img"));
@@ -33,4 +34,28 @@ function makePageForEpisodes(episodeList) {
   });
 };
 
+/*Search bar*/
+const searchBar = document.getElementById("searchBar");
+searchBar.addEventListener("input", filterSearch);
+
+/*Filter*/
+function filterSearch(e) {
+  e.preventDefault();
+  let userInput = e.target.value.toLowercase();
+  let filteredResults = allEpisodes.filter((episode) => {
+    return (
+      episode.name.toLowercase().includes(userInput) ||
+      episode.summary.toLowercase().includes(userInput)
+    );
+  });
+  makePageForEpisodes(filteredResults);
+}
 window.onload = setup;
+
+/* <!-- Pseudo Code
+Add a "live" search input:
+Only episodes whose summary OR name contains the search term should be displayed
+The search should be case-insensitive
+The display should update immediately after each keystroke changes the input.
+Display how many episodes match the current search
+If the search box is cleared, all episodes should be shown. --> */
